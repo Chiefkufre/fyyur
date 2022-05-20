40006a0c86f2b965f854ad62d6ef4f8a94d7d0b3
chiefkufre1 <kufhub@gmail.com>
2022-05-20 10:17:00 +0100

    chore: remove models.py to teporary solve cylic import bugs

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 #----------------------------------------------------------------------------#
 
 import json
-import dateutil.parser
+import dateutil
 import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
@@ -13,6 +13,7 @@ from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
 from flask_migrate import Migrate
+from models import Artist, ArtistGenre, Venue, VenueGenre, Show
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -27,9 +28,73 @@ migrate = Migrate(app, db)
 
 
 #----------------------------------------------------------------------------#
-# Models-move to models.py
+# Models
 #----------------------------------------------------------------------------#
 
+class ArtistGenre(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey("artists.id"), nullable=False)
+    genre = db.Column(db.String(50), nullable=False)
+    
+    def __repr__(self):
+        return f"<Artist_Genre artist_id:{self.artist_id} genre: {self.genre}>"
+    
+
+  
+
+
+class Artist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True )
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    facebook_link = db.Column(db.String(120), nullable=True)
+    venues = db.relationship("Venue", secondary="shows", backref=db.backref("artists", lazy=True))
+    seeking_venue = db.Column(db.Boolean, nullable=True, default=False)
+    seeking_description = db.Column(db.String(), nullable=True, default="")
+    genres = db.relationship("Artist_Genre", backref="artist", lazy=True)
+    image_link = db.Column(
+        db.String(500),
+        nullable=True,
+        default="https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+    )
+   
+    
+
+class Show(db.Model):
+    artist_id = db.Column(db.Integer, db.ForeignKey("artists.id"), primary_key=True)
+    venue_id = db.Column(db.Integer, db.ForeignKey("venues.id", ondelete="CASCADE"), primary_key=True)
+    start_time = db.Column(db.DateTime, nullable=False)
+    
+
+class VenueGenre(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    venue_id = db.Column(db.Integer, db.ForeignKey("venues.id", ondelete="CASCADE"), nullable=False )
+    genre = db.Column(db.String(50), nullable=False)
+    
+    def __repr__(self):
+        return f"<Venue_Genre venue_id:{self.venue_id} genre: {self.genre}>"
+
+
+
+class Venue(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True )
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=True)
+    genres = db.relationship("Venue_Genre", passive_deletes=True, backref="venue", lazy=True)
+    seeking_talent = db.Column(db.Boolean, nullable=True, default=False)
+    website = db.Column(db.String(120), nullable=True)
+    seeking_description = db.Column(db.String(120), nullable=True)
+    image_link = db.Column(db.String(500), nullable=True,
+        default="https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+    )
+    facebook_link = db.Column(db.String(120), nullable=True, default="www.facebook.com/samuelkufrewillie")
 
 #----------------------------------------------------------------------------#
 # Filters.
@@ -194,14 +259,48 @@ def create_venue_form():
 @app.route('/venues/create', methods=['POST'])
 def create_venue_submission():
   # TODO: insert form data as a new Venue record in the db, instead
+  form = VenueForm()
   # TODO: modify data to be the data object returned from db insertion
+  venue = Venue.query.filter_by(email=form.email.data).first()
+  if venue:
+    flash(f'Sorry, an venue is is already listed with this email: {form.email.data}')
+    return render_template('pages/home.html')
+  elif form.validate_on_submit():
+    fname = form.name.data.strip()
+    femail = form.email.data
+    fphone = form.phone.data.strip()
+    fcity = form.city.data
+    fstate = form.state.data
+    faddress = form.address.data
+    fwebsite = form.website_link.data
+    fimage = form.image_link.data
+    fgenres = form.genres.data                  
+    ftalent = True if form.seeking_talent.data == 'Yes' else False
+    fdescription = form.seeking_description.data.strip()
+    ffacebook = form.facebook_link.data.strip()
+    
+    try:
+      new_venue = Venue(name=fname, email=femail, phone=fphone,city=fcity,
+                      state=fstate, address=faddress, website_link=fwebsite, 
+                      image_link=fwebsite, genres=fgenres, seeking_talent=ftalent, 
+                      seeking_description=fdescription, facebook_link=ffacebook)
+      db.session.add(new_venue)
+      db.session.commit()
+      # on successful db insert, flash success
+      flash('The Venue ' + fname + ' was successfully listed!')
+    
+    except Exception as e:
+            # TODO: on unsuccessful db insert, flash an error instead.
+      flash('An error occurred. Venue ' + fname + ' could not be listed. Please try again')
+      db.session.rollback()
+    finally:
+      db.session.close()
+  
 
-  # on successful db insert, flash success
-  flash('Venue ' + request.form['name'] + ' was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Venue ' + data.name + ' could not be listed.')
-  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
-  return render_template('pages/home.html')
+  else:
+    flash( form.errors )
+    return redirect(url_for('create_venue_submission'))
+  # return render_template('pages/home.html')
 
 @app.route('/venues/<venue_id>', methods=['DELETE'])
 def delete_venue(venue_id):

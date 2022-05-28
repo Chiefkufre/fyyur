7e57891b559a0d1c201ccc36871ec724f67630c6
chiefkufre1 <kufhub@gmail.com>
2022-05-29 00:54:58 +0100

    fix bugs

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from settings import DB_NAME, DB_PASSWORD, DB_USER
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -10,4 +11,4 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:@localhost:5432/fyyurdb'
+SQLALCHEMY_DATABASE_URI = 'postgresql://DB_USER:DB_PASSWORD@localhost:5432/DB_NAME'
